e80729091cc661d9de451a26b014a7c84f152439
adimenta <adimenta@7e029f7e-3e8b-a1f4-4c34-3238ba50a184>
2011-11-27 22:17:56 +0000

    Map With Overlay

---
+++ GOOD2GO/src/com/gdma/good2go/G2GItemizedOverlay.java
@@ -0,0 +1,65 @@
+package com.gdma.good2go;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+
+/*This class can manage a whole set of Overlay (which are the individual items placed on the map)*/
+public class G2GItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+	
+	/*The array in which we'll put each of the OverlayItem objects we want on the map*/
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	
+	/*a reference to the application Context - to handle touch events on the overlay items*/
+	private Context mContext;
+	
+	/*The constructor must define the default marker for each of the OverlayItems. 
+	 * In order for the Drawable to actually get drawn, it must have its bounds defined. 
+	 * Most commonly, you want the center-point at the bottom of the image to be the point at which it's attached to the map coordinates. 
+	 * This is handled for you with the boundCenterBottom() method.
+	 * Plus initialize context*/
+	public G2GItemizedOverlay(Drawable defaultMarker, Context context) {
+		  super(boundCenterBottom(defaultMarker));
+		  mContext = context;
+		}
+
+
+	/*In order to add new OverlayItems to the ArrayList*/
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    /*Each time you add a new OverlayItem to the ArrayList, you must call populate() for the ItemizedOverlay, 
+	     * which will read each of the OverlayItems and prepare them to be drawn*/
+	    populate();
+	}
+	
+	/*When the populate() method executes, it will call createItem(int) in the ItemizedOverlay to retrieve each OverlayItem. 
+	 * You must override this method to properly read from the ArrayList and return the OverlayItem from the position specified by the given integer*/
+	@Override
+	protected OverlayItem createItem(int i) {
+	  return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+	  return mOverlays.size();
+	}
+	
+	/*onTap(int) callback method, which will handle the event when an item is tapped by the user*/
+	@Override
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  /*This uses the member android.content.Context to create a new AlertDialog.Builder*/
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  /*uses the tapped OverlayItem's title and snippet for the dialog's title and message text*/
+	  dialog.setTitle(item.getTitle());
+	  dialog.setMessage(item.getSnippet());
+	  dialog.show();
+	  return true;
+	}
+}
