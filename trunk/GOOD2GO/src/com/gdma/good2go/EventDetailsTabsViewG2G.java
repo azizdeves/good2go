e7d71987ebd6841734cc4c97d1c2b49a64ae4628
adimenta <adimenta@7e029f7e-3e8b-a1f4-4c34-3238ba50a184>
2011-12-08 22:28:02 +0000

    data is passing between screens (leaded from internal DB)

--- trunk/GOOD2GO/src/com/gdma/good2go/EventDetailsTabsViewG2G.java
+++ trunk/GOOD2GO/src/com/gdma/good2go/EventDetailsTabsViewG2G.java
@@ -2,14 +2,25 @@ package com.gdma.good2go;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class EventDetailsTabsViewG2G extends TabActivity {
 	
     TabHost mTabHost;
+    
+    private String mRowId;
+    private String mEventName;
+    private String mEventDesc;
+    private String mEventDetails;
+    
+    private EventsDbAdapter mDbHelper;
+    
+    /**@TODO - add all the fields here in case this activity dies, so it can resume
   
 	
 	/** Called when the activity is first created. */
@@ -19,11 +30,48 @@ public class EventDetailsTabsViewG2G extends TabActivity {
 	    setContentView(R.layout.event_details);
 
 	    mTabHost = getTabHost();
+	    
+	    /**GET DATA PASSED FROM BALOON*/
+	    Bundle extras = getIntent().getExtras();
+	    mRowId=(extras!=null)?extras.getString("rowid"):null;
 
 	    
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_about_event").setIndicator("About").setContent(R.id.textview1));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_map_event").setIndicator("Map").setContent(R.id.textview2));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_npo").setIndicator("Who").setContent(R.id.textview3));
+	    /**GET DATA FROM DB*/
+	    mDbHelper = new EventsDbAdapter(this);
+	    mDbHelper.open();
+	    
+	    Cursor event = mDbHelper.fetchEvent(Long.parseLong(mRowId));
+	    startManagingCursor(event);
+	    
+	    /**POPULATE VIEWS FROM DB*/   
+	    TextView eventTitle = (TextView) findViewById(R.id.eventTitle);
+	    TextView eventDescription = (TextView) findViewById(R.id.eventDescription);
+	    TextView eventDetails = (TextView) findViewById(R.id.eventdetails);
+
+	    
+	    mEventName=event.getString(event.getColumnIndexOrThrow
+	    		(EventsDbAdapter.KEY_EVENTNAME));
+	    mEventDesc=event.getString(event.getColumnIndexOrThrow(
+	    		EventsDbAdapter.KEY_EVENT_SHORT_INFO));
+	    mEventDetails=event.getString(event.getColumnIndexOrThrow
+	    		(EventsDbAdapter.KEY_EVENT_DETAILS));
+	    
+	    eventTitle.setText(mEventName);
+	    eventDescription.setText(mEventDesc);
+	    eventDetails.setText(mEventDetails);
+
+	    if(event!=null&&!event.isClosed()){
+        	mDbHelper.close();
+        }
+	    
+	    
+	    /**SHOW TABS AND LAYOUT*/
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_about_event").setIndicator
+	    		("About").setContent(R.id.eventdetails));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_map_event").setIndicator(
+	    		"Map").setContent(R.id.textview2));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_npo").setIndicator
+	    		("Who").setContent(R.id.textview3));
 	    
 	    mTabHost.setCurrentTab(0);
 	    
@@ -32,12 +80,14 @@ public class EventDetailsTabsViewG2G extends TabActivity {
 	   
 	    buttonCountMeIn.setOnClickListener(new View.OnClickListener() {
 	        public void onClick(View view) {
-	           // mSoundManager.playSound(3);
+	    		Bundle extraInfo = new Bundle();
+	            extraInfo.putString("name", mEventName);
+	            extraInfo.putString("desc", mEventDesc);
+	            
 	            Intent newIntent = new Intent(view.getContext(), 
 	                            CountMeIn.class);
+	            newIntent.putExtras(extraInfo);
 	            startActivityForResult(newIntent, 1);
-	    		//Toast.makeText(view.getContext(), "onButtonClick Register",
-				//Toast.LENGTH_LONG).show();
 	        }
 	    });
 	}
