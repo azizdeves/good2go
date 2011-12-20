3fbeed33c42df7277c71d4919b2b1d128a25ed28
ygv.mor@gmail.com <ygv.mor@gmail.com@7e029f7e-3e8b-a1f4-4c34-3238ba50a184>
2011-12-20 14:21:55 +0000

    Added: 	Filter button to list view

--- trunk/GOOD2GO/src/com/gdma/good2go/ListTab.java
+++ trunk/GOOD2GO/src/com/gdma/good2go/ListTab.java
@@ -1,16 +1,26 @@
 package com.gdma.good2go;
 
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 
 public class ListTab extends ListActivity {
 	
+	private static final int GET_FILTERED_EVENTS = 10;
 	private EventsDbAdapter mDbHelper;
 	Cursor mEventsCursor;
 	
@@ -25,22 +35,51 @@ public class ListTab extends ListActivity {
     	
     	/**GET POINTS FROM LOCAL DB*/
     	mDbHelper = new EventsDbAdapter(this);
-		mDbHelper.open();
+		mDbHelper.open(); //TODO CLOSE WHEN THE APP IS CLOSED
+		
 		mEventsCursor = mDbHelper.fetchAllEvents();
 		startManagingCursor(mEventsCursor);
-		
-		
-		/**SHOW THE POINTS IN THE LIST*/
-        mColumns = new String[] {EventsDbAdapter.KEY_EVENTNAME, 
-        		EventsDbAdapter.KEY_EVENT_SHORT_INFO,EventsDbAdapter.KEY_EVENT_DISTANCE};
+	
+		showPointsInList();
+        setContentView(R.layout.list);
+		final Button buttonFilterEvents = (Button) findViewById(R.id.FilterEventsListViewButton);
+    }
+    
+    private void showPointsInList(){
+        mColumns = new String[] {EventsDbAdapter.KEY_EVENTNAME, EventsDbAdapter.KEY_EVENT_SHORT_INFO,EventsDbAdapter.KEY_EVENT_DISTANCE};
         int[] to = new int[] {R.id.eventname_entry, R.id.eventinfo_entry, R.id.eventdistance_entry };
-
-        SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(this, 
-        		R.layout.list_item, 
-        		mEventsCursor, mColumns, to);
-
+        SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(this,R.layout.list_item,mEventsCursor, mColumns, to);
         setListAdapter(mAdapter);
-		}
+        
+    }
+    
+    @Override
+ 	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+ 	{
+ 		super.onActivityResult(requestCode, resultCode, data);
+ 		 if (requestCode == GET_FILTERED_EVENTS) {
+ 			if(resultCode==RESULT_OK){
+ 				Bundle bundleResult = data.getExtras();
+ 				int i=0;
+ 				int duration=0;
+ 				int radius=0;
+ 				String types = (bundleResult!=null) ? bundleResult.getString("1"):null;
+// 				Toast debugging=Toast.makeText(this,types, Toast.LENGTH_LONG);
+// 				debugging.show();	
+ 				duration= (bundleResult!=null) ? bundleResult.getInt("durationInMinutes"): -1;
+ 				radius = (bundleResult!=null) ? bundleResult.getInt("radius"):-1;
+
+ 				mEventsCursor = mDbHelper.fetchAllEvents();
+ 				//mEventsCursor = mDbHelper.fetchEventByFilters(types, radius, duration)
+ 				startManagingCursor(mEventsCursor);
+ 				showPointsInList();
+ 			}
+ 			if(resultCode==RESULT_CANCELED){
+			
+ 			}		
+ 		 }
+ 	}
+    
     
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -54,4 +93,9 @@ public class ListTab extends ListActivity {
         i.putExtra(EventsDbAdapter.KEY_EVENTID, id);
         startActivity(i);
     }
+    
+	public void getFilterScreen(View view){
+    	Intent myIntent = new Intent(view.getContext(),FilterTab.class);
+    	startActivityForResult(myIntent, GET_FILTERED_EVENTS);		
+	}
 }
