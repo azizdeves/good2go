bc4c07589a31adb8ce43c848b5f0302827e39605
adimenta <adimenta@7e029f7e-3e8b-a1f4-4c34-3238ba50a184>
2012-01-13 23:14:04 +0000

    user location in map and event start/end time APK

--- GOOD2GO/src/com/gdma/good2go/ui/maputils/UserItemizedOverlay.java
+++ GOOD2GO/src/com/gdma/good2go/ui/maputils/UserItemizedOverlay.java
@@ -2,26 +2,17 @@ package com.gdma.good2go.ui.maputils;
 
 import java.util.ArrayList;
 
-import android.app.AlertDialog;
-import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.widget.Toast;
 
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
 
 public class UserItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
-	Context mContext;
 
 	public UserItemizedOverlay(Drawable defaultMarker) {
 		super(boundCenterBottom(defaultMarker));
 	}
-	
-	public UserItemizedOverlay(Drawable defaultMarker, Context context) {
-		  super(defaultMarker);
-		  mContext = context;
-		}
 
 	@Override
 	protected OverlayItem createItem(int i) {
@@ -36,13 +27,6 @@ public class UserItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 	public void addOverlay(OverlayItem overlay) {
 	    mOverlays.add(overlay);
 	    populate();
-	}
-	
-	@Override
-	protected boolean onTap(int index) {
-	  OverlayItem item = mOverlays.get(index);
-	  Toast.makeText(mContext, item.getTitle(), Toast.LENGTH_SHORT).show();
-	  return true;
 	}
 	
 }
